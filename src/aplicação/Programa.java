ad2f7605d082bab2d5e81866e2d1752b57356fa1
Gustavo Santana <gustavoo_santana@hotmail.com>
2019-11-07 15:53:25 -0300

    pior solução

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,53 @@
+package aplicação;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import modelo.entidades.Reserva;
+
+public class Programa {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.print("Numero Quarto: ");
+		int numero = sc.nextInt();
+		System.out.print("Data Check-In: ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Data Check-Out: ");
+		Date checkOut = sdf.parse(sc.next());
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Erro ao fazer a Reserva: Data do Check-Out anterior a data do Check-In!");
+		} else {
+			Reserva reserva = new Reserva(numero, checkIn, checkOut);
+			System.out.println("Reserva: " + reserva);
+
+			System.out.println();
+			System.out.println("Informe a nova data da Reserva:");
+			System.out.print("Data Check-In: ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Data Check-Out: ");
+			checkOut = sdf.parse(sc.next());
+
+			Date agora = new Date();
+			if (checkIn.before(agora) || checkOut.before(agora)) {
+				System.out.println("Erro ao fazer a Reserva: Datas informadas deverão ser futuras!");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("Erro ao fazer a Reserva: Data do Check-Out anterior a data do Check-In!");
+			} else {
+				reserva.atualizaData(checkIn, checkOut);
+				System.out.println("Reserva: " + reserva);
+			}
+
+		}
+
+		sc.close();
+
+	}
+
+}
